099976e9f9a85b264a9f44944420519f41cf8e05
Marvin EDORH <83826055+MarvinEdorh@users.noreply.github.com>
2021-09-09 11:05:32 +0200

    Update SQL_ITW_DM.sql

--- SQL_ITW_DM.sql
+++ SQL_ITW_DM.sql
@@ -37,12 +37,18 @@ SELECT campaign_id FROM Ads WHERE status = "active"
 
 --3) The number of events per each ad — ad_id, event_type, by event type.
 
-SELECT Ads.ad_id, event_type, COUNT(ad_id) FROM Ads 
+SELECT Ads.ad_id, event_type, COUNT(event_id) FROM Ads 
 FULL JOIN Events
 USING (ad_id)
-GROUP BY ad_id, event_type,
+GROUP BY Ads.ad_id, event_type,
 
 --4) The number of events over the last week per each active ad — broken down by event type and date (most recent first).
+
+SELECT Ads.ad_id, COUNT(event_id) FROM Ads 
+FULL JOIN Events
+USING (ad_id)
+WHERE status = "active" AND date > DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
+GROUP BY Ads.ad_id
 
 ------------------------
 ------- LEVEL 2  -------
